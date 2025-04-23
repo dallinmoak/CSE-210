64f295ebd8e3795046d56c8cec9d56f859e1007d
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-04-23 12:48:23 -0600

    w01 exercise 1 complete

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,12 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
+  static void Main(string[] args)
+  {
+    Console.Write("What is your first name? ");
+    string f_name = Console.ReadLine();
+    Console.Write("What is your last name? ");
+    string l_name = Console.ReadLine();
+    Console.Write($"\nYour name is {l_name}, {f_name} {l_name}.\n");
+  }
 }
