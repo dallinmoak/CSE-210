82c3751d985dd493ce749bc4382c502d5202efbf
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-04-23 15:02:28 -0600

    exercise 3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,65 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    // outer loop, for playing another round of guessing
+    bool again = true;
+    Console.Write("Welcome to the guessing game.\n");
+    while (again)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+      //play game
+      //  inner loop for guessing
+      bool stillGuessing = true;
+      int guessCount = 0;
+      int target = new Random().Next(1, 101);
+      while (stillGuessing)
+      {
+        Console.Write("Guess a number between 1 and 100: ");
+        // second inner loop for collecting valid inputs
+        bool validInput = false;
+        while (!validInput)
+        {
+          string guessRaw = Console.ReadLine();
+          if (!int.TryParse(guessRaw, out int currentGuess) || currentGuess < 1 || currentGuess > 100)
+          {
+            Console.WriteLine("Bad input. Enter an integer between 1 and 100.");
+            continue;
+          }
+          else
+          {
+            validInput = true;
+            guessCount++;
+            // check if the guess is correct
+            if (currentGuess == target)
+            {
+              Console.WriteLine($"Correct. The number was {target}. It took you {guessCount} tries.");
+              stillGuessing = false;
+            }
+            else if (currentGuess < target)
+            {
+              Console.WriteLine($"guess of {currentGuess} is too low. Try again.");
+            }
+            else
+            {
+              Console.WriteLine($"guess of {currentGuess} is too high. Try again.");
+            }
+          }
+        }
+      }
+      // end play game
+
+      // outer loop control:
+      Console.Write("Do you want to play again? (Y/n): ");
+      string againRes = Console.ReadLine().ToLower();
+      if (againRes == "n")
+      {
+        again = false;
+      }
+      else
+      {
+        Console.Write("\n\n");
+      }
     }
+  }
 }
