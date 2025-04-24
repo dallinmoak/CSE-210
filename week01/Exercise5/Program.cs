7377c251d770c44211ef55c3964cdd66bbe1b43e
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-04-24 16:09:35 -0600

    exercise 5 complete

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void DisplayWelcome()
+  {
+    Console.Write("Welcome to the program!");
+  }
+  static string PromptUserName()
+  {
+    Console.Write("Please enter your name: ");
+    return Console.ReadLine();
+  }
+  static int PromptUserNumber()
+  {
+    while (true)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+      Console.Write("Please enter your favorite number: ");
+      string raw = Console.ReadLine();
+      if (int.TryParse(raw, out int num))
+      {
+        return num;
+      }
+      Console.Write("Not an Int. try again: ");
     }
+  }
+  static int SquareNumber(int num)
+  {
+    return num * num;
+  }
+  static void DisplayResult(string name, int square)
+  {
+    Console.Write($"{name}, the square of your number is {square}");
+  }
+  static void Main(string[] args)
+  {
+    DisplayWelcome();
+    string name = PromptUserName();
+    int num = PromptUserNumber();
+    DisplayResult(name, SquareNumber(num));
+  }
 }
