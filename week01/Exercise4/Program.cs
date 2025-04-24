0f404bb3528ad389a44a3a4382bf6dd4d447cac6
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-04-24 08:39:15 -0600

    exercise 4 complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    bool again = true;
+    while (again)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+      Console.Write("Enter a list of numbers, type 0 when finished.\n");
+      List<int> numbers = [];
+      bool stillAdding = true;
+      while (stillAdding)
+      {
+        Console.Write("Enter a number: ");
+        string rawNum = Console.ReadLine();
+        if (int.TryParse(rawNum, out int num) && num >= 0)
+        {
+          if (num == 0)
+          {
+            stillAdding = false;
+          }
+          else
+          {
+            numbers.Add(num);
+          }
+        }
+      }
+      Console.Write("The list is: [");
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        Console.Write(numbers[i]);
+        if (i != numbers.Count - 1)
+        {
+          Console.Write(", ");
+        }
+      }
+      Console.Write("]\n");
+      int sum = 0;
+      foreach (int num in numbers)
+      {
+        sum += num;
+      }
+      Console.Write($"The sum is: {sum}\n");
+      float fSum = sum;
+      float fCount = numbers.Count;
+      float average = fSum / fCount;
+      Console.Write($"The average is: {average}\n");
+      int max = 0;
+      foreach (int num in numbers)
+      {
+        if (num > max)
+        {
+          max = num;
+        }
+      }
+      Console.Write($"The maximum is: {max}\n");
+
+      // outer loop control:
+      Console.Write("Do you want to play again? (Y/n): ");
+      string againRes = Console.ReadLine().ToLower();
+      if (againRes == "n")
+      {
+        again = false;
+      }
+      else
+      {
+        Console.Write("\n\n");
+      }
     }
+  }
 }
