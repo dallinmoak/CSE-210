25c9648e2425f8f3383d41c48efa3adf74f91604
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-05-25 16:55:30 -0600

    standard life cylce for the listing activity

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,7 +1,18 @@
 class ListingActivity : Activity
 {
+
+  public ListingActivity()
+  {
+    this._type = "Listing";
+    this._description = "This activity is where you make a list of things about a topic.";
+    base.PreRun();
+  }
+
   public void Run()
   {
-
+    Console.Write("this is the listing activity ");
+    base.Spin(2000);
+    Console.Write("that's all for listing\n");
+    base.PostRun();
   }
 }
