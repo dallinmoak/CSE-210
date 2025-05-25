377e5153b095f109c2180e9aaafdb7321500790f
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-05-25 16:58:32 -0600

    reflection activity basic life cycle

--- week05/Mindfulness/ReflectionActivity.cs
+++ week05/Mindfulness/ReflectionActivity.cs
@@ -1,10 +1,17 @@
 class ReflectionActivity : Activity
 {
+  public ReflectionActivity()
+  {
+    this._type = "Reflection";
+    this._description = "Now you get to read some stuff and think about it.";
+    base.PreRun();
+  }
+
   public void Run()
   {
-    Console.Write("running reflection activity...\n");
+    Console.Write("this is the reflection activity ");
     base.Spin(2000);
-    Console.Write("reflection activity done");
-    Console.Clear();
+    Console.Write("that's all for reflection\n");
+    base.PostRun();
   }
 }
