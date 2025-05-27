ee554f8ef28a6eebf32a8983c2c35c0dfdb687d2
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-05-26 21:09:50 -0600

    listing activity starting point

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,11 +1,20 @@
 class ListingActivity : Activity
 {
 
+  private List<string> _prompts;
+
   public ListingActivity()
   {
     this._type = "Listing";
-    this._description = "This activity is where you make a list of things about a topic.";
+    this._description = "This activity is where you make a list of things about a topic. when prompted, type the list items separated by newlines. The last newline you send past the activity duration will end the activity.";
     base.PreRun();
+    this._prompts = new List<string>
+    {
+      "List all the members of the Q Continuum you can think of",
+      "List all the Klingon words you know",
+      "List the name and serial number of every Starfleet ship you know",
+      "List the episodes in TNG in which Security Chief Tasha Yar dies (hint: it's more than 1)",
+    };
   }
 
   public void Run()
