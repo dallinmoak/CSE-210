1ebf73592ad91101d3101769367f0c729b390212
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-06-07 14:05:39 -0600

    bones of an Activity Tracker class def

---
+++ week07/ExerciseTracking/ActivityTracker.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+
+class ActivityTracker
+{
+
+  private string _ownerName;
+  private string _preferredUnit;
+  private List<Activity> _activities;
+
+  public ActivityTracker(string ownerName, string preferredUnit, List<Activity> activities)
+  {
+    this._ownerName = ownerName;
+    this._preferredUnit = preferredUnit;
+    this._activities = activities;
+  }
+
+  public void PrintSummary()
+  {
+    Console.Write($"Here's the activity set for {_ownerName} in {_preferredUnit}:\n");
+    foreach (Activity activity in _activities)
+    {
+      Console.WriteLine(activity.GetSummary(_preferredUnit));
+    }
+  }
+}
