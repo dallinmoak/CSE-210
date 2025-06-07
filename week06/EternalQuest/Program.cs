c669ad36d2c3861a82a574ca197d24431197dbc2
Dallin Moak (MSI ubuntu) <dallinmoak@gmail.com>
2025-06-06 21:49:07 -0600

    clarify program entry file and add introductory comments

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,10 +1,15 @@
 using System;
-using System.Net.Quic;
 
 class Program
 {
   static void Main(string[] args)
   {
+    // Hi B. Parish, Here's my program for the goal manager. I didn't quite get around to implementing the saving feature, but the loading feature, while not usable, is fleshed out and theoretically works.
+
+    // I feel my flow control design shows extra creativity, including the way a user can functionally navigate between 3 different types of menus, and the use of a GoalMaker class for single-point access to user-driven goal creation.
+    // also of merit is my use of overloads and alternate contructors to allow for either user driven or file-driven goal creation.
+
+    // see my commit history for evidence of the organic development process
     Console.Write("Welcome to the goal manager\n");
     bool quit = false;
     while (!quit)
@@ -27,14 +32,14 @@ class Program
         pathPrompt = "please enter the path where the goal set file is located: ";
       }
       Console.Write(pathPrompt);
-      string location = Console.ReadLine();
-      goalSet = new GoalSet(location);
+      string filePath = Console.ReadLine();
+      goalSet = new GoalSet(filePath);
       bool close = false;
       while (!close)
       {
         close = goalSet.ShowMenu();
       }
-      Console.Write("you've closed the goal set at " + location + "\n");
+      Console.Write("you've closed the goal set at " + filePath + "\n");
     }
   }
 }
